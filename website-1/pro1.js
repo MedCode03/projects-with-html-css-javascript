1cb512bf5f16c174e1e72d969456ac2ca32e9d2b
MedWeb <72758665+MedCode03@users.noreply.github.com>
2023-01-03 17:37:06 +0100

    Add files via upload

---
+++ website-1/pro1.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("ham");
+var y = document.getElementById("list");
+var z = document.getElementById("dlt");
+x.onclick = () => {
+   y.style.display= "block"; 
+   
+}
+z.onclick = () => {
+  y.style.display= "none"; 
+  
+}
+
+
+
+
+
+
